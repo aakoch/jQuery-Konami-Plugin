30e521a3ff17d4de23729d178b146b06c9ac56c9
unknown <Igen005@.upcorp.ad.uprr.com>
2010-09-07 11:31:03 -0500

    added comment with license

--- konami.js
+++ konami.js
@@ -1,3 +1,13 @@
+/**
+ * jQuery Konami Plugin 0.1 (2009-10-20)
+ *
+ * Written by Adam A. Koch
+ * http://adamkoch.com
+ *
+ * Licensed under the WTFPL (http://sam.zoy.org/wtfpl/)
+ *
+ * Requires jQuery http://jquery.com (1.3.2 at time of release)
+ */
 (function ($) {
     $.konami = function (callback) {
         var code = "38,38,40,40,37,39,37,39,66,65";
